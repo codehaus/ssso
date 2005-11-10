7d8ba55c7b35ad9f77e3eabedb2aa9e5760f94d0
damonrand <damonrand@cf848898-e501-0410-83e0-dbd095534409>
2005-11-10 09:08:54 +0000

    Updated docs

--- org.codehaus.ssso/src/org/codehaus/ssso/provider/ISSOTokenProvider.java
+++ org.codehaus.ssso/src/org/codehaus/ssso/provider/ISSOTokenProvider.java
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 2005, Damon Rand
- * 
+ *
  * Licensed subject to: http://ssso.codehaus.org/ContributorAgreement
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to permit
  * persons to whom the Software is furnished to do so, subject to the
  * following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
@@ -30,19 +30,18 @@ import java.net.URL;
 import org.codehaus.ssso.token.ISimpleSSOToken;
 
 /**
- * DoorKeeperModules can authenticate username/password and create a Domino
- * ltpatoken
- * 
+ * ISSOTokenProvider implementations return ISimpleSSOToken objects credentials
+ *
  * @author drand
- * 
+ *
  */
 public interface ISSOTokenProvider {
 
     /**
      * AuthenticationException is thrown when a user can't be authenticated.
-     * 
+     *
      * @author drand
-     * 
+     *
      */
     public class AuthenticationException extends RuntimeException {
 
@@ -58,9 +57,9 @@ public interface ISSOTokenProvider {
     /**
      * Tries to log in a user based on name and password. If no exception is
      * thrown the authentication was successful.
-     * 
+     *
      * @throws IOException
-     * 
+     *
      * @throws AuthenticationException
      *             If the user could not be authenticated.
      */
