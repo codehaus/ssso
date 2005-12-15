2ea196d2c3287e001dea6b7388558dae977135ce
damonrand <damonrand@cf848898-e501-0410-83e0-dbd095534409>
2005-12-15 21:19:49 +0000

    - Make sure dependencies are availble in SVN

--- org.codehaus.ssso/src/common/org/codehaus/ssso/ISimpleSSOPrincipal.java
+++ org.codehaus.ssso/src/common/org/codehaus/ssso/ISimpleSSOPrincipal.java
@@ -1,8 +1,13 @@
 package org.codehaus.ssso;
 
-
 import java.security.Principal;
 
+/**
+ * TODO Rename this to ISimpleSSOUser
+ * 
+ * @author drand
+ * 
+ */
 public interface ISimpleSSOPrincipal extends Principal {
 
     public String getEmail();
